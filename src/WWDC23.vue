7cb5adb487dae309f4bb3f184bcd5f267880edfd
EyreFree <eyrefree@eyrefree.org>
2023-08-27 23:49:36 +0800

    fix url

--- src/WWDC23.vue
+++ src/WWDC23.vue
@@ -1,4 +1,4 @@
 
 <template>
-  <iframe style="border:none; height: 100%; width: 100%;" src="https://wwdc23.vercel.app/?lang=en"></iframe>
+  <iframe style="border:none; height: 100%; width: 100%;" src="https://wwdc23.swiftgg.team/?lang=en"></iframe>
 </template>
--- src/WWDCPlayground.js
+++ src/WWDCPlayground.js
@@ -16,7 +16,7 @@ class Playground {
 }
 
 export const activities = [
-  new Playground(wwdc23CoverImg, "WWDC23.playground", "SwiftGG", "2023 年 6 月 6 日 - 6 月 10 日", "20 名嘉宾", "已结束", "./wwdc23"),
-  new Playground(wwdc22CoverImg, "WWDC22.playground", "SwiftGG", "2022 年 6 月 7 日 - 6 月 11 日", "27 名嘉宾", "已结束", "./wwdc22"),
-  new Playground(wwdc21CoverImg, "WWDC21.playground", "SwiftGG", "2021 年 6 月 8 日 - 6 月 12 日", "13 名嘉宾", "已结束", "./wwdc21")
+  new Playground(wwdc23CoverImg, "WWDC23.playground", "SwiftGG", "2023 年 6 月 6 日 - 6 月 10 日", "20 名嘉宾", "已结束", "https://wwdc23.swiftgg.team"),
+  new Playground(wwdc22CoverImg, "WWDC22.playground", "SwiftGG", "2022 年 6 月 7 日 - 6 月 11 日", "27 名嘉宾", "已结束", "https://wwdc22.swiftgg.team"),
+  new Playground(wwdc21CoverImg, "WWDC21.playground", "SwiftGG", "2021 年 6 月 8 日 - 6 月 12 日", "13 名嘉宾", "已结束", "https://wwdc21.swiftgg.team")
 ]
