41a4a76fc80e55f7e7b5b9d846138a0d7ce1d06f
onee <onee.me@outlook.com>
2023-10-03 21:06:58 +0800

    add vercel analystics

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
-  import NavHeader from './components/NavHeader.vue'
-  import MobileMenu from './components/MobileMenu.vue'
-  import Hero from './components/Hero.vue'
-  import Projects from './components/Projects.vue'
-  import WWDCPlayground from './components/wwdc_pg/WWDCPlayground.vue'
-  import SwiftManual from './components/SwiftManual.vue'
-  import GGTalk from './components/GGTalk.vue'
-  import Partner from './components/Partner.vue'
-  import Social from './components/Social.vue'
-  import Footer from './components/Footer.vue'
+import NavHeader from './components/NavHeader.vue'
+import MobileMenu from './components/MobileMenu.vue'
+import Hero from './components/Hero.vue'
+import Projects from './components/Projects.vue'
+import WWDCPlayground from './components/wwdc_pg/WWDCPlayground.vue'
+import SwiftManual from './components/SwiftManual.vue'
+import GGTalk from './components/GGTalk.vue'
+import Partner from './components/Partner.vue'
+import Social from './components/Social.vue'
+import Footer from './components/Footer.vue'
+import { inject } from '@vercel/analytics';
 
-  import orangeArrow from './assets/orange-arrow-left.png'
-  import orangePlanet from './assets/orange-planet-right.png'
+
+import orangeArrow from './assets/orange-arrow-left.png'
+import orangePlanet from './assets/orange-planet-right.png'
+
+inject();
 </script>
 
 <template>
