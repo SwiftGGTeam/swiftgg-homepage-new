9b077017dc9dd41474e07f5a2a669bce971a5914
onee <onee.me@outlook.com>
2023-11-06 21:37:17 +0800

    add company

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -37,14 +37,13 @@ import SwiftGGLogo from '../assets/swiftgg-logo.png'
 
       <div class="px-1 my-5 border-t border-gray-300"></div>
       <div class="flex flex-col justify-center items-center text-sm text-slate-500 italic gap-2">
-        <p class="font-mono">Copyright &copy; SwiftGG <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023104442号-1</a></p>
+        <p class="font-mono">SwiftGG &copy; <a href="https://xreality.zone/zh/pages/company" target="_blank">深圳市虚实基地科技有限公司</a> <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023104442号-1</a></p>
         <p class="font-mono">11 crafted by  
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="inline w-4 h-4 text-red-600">
             <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
           </svg>.
         </p>
       </div>
-      
     </container>
   </div>
 </template>
